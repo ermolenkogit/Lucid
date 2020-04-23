e7aa1e5ecfc5a2433c1ce4f113893b6c94ecac89
Ermolenko-Sergey <ermolenko.sergey.petrovich@gmail.com>
2020-04-23 21:20:49 +0300

    Test commit.

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+// Настройка карусели
+
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+        // autoplay:true,
+        autoplayTimeout: 5000,
+        smartSpeed: 1500,
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+// Настройка butoon up
+
+$(document).ready(function() {
+    var button = $('#button-up');
+    $(window).scroll (function () {
+        if ($(this).scrollTop () > 300) {
+            button.fadeIn();
+        } else {
+            button.fadeOut();
+        }
+    });
+    button.on('click', function(){
+        $('body, html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+});
